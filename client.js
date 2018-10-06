e1726165477a9dd932bcec1b5e204d4c9537afc2
William Harrison <wm.harrison@gmail.com>
2018-10-06 17:08:35 -0500

    finished up project. need to refactor delete function)

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 console.log('JS');
 
 let empArr = [];
+let empSal = 0;
 
 class Employee {
     constructor( fName, lName, id, title, annual ) {
@@ -17,6 +18,7 @@ $('document').ready(onReady);
 function onReady() {
     console.log('JQ');
     $('#addNewEmp').on('click', addEmployee);
+    $('#newEmp').on('click', '.deleteEmp', deleteEmp);
 }
 
 // add employee function
@@ -33,7 +35,18 @@ function addEmployee() {
     empArr.push( newEmployee );
     console.log(empArr);
     appendEmployeeList();
+    resetInputs();
     sumSal();
+}
+
+// reset input fields
+
+function resetInputs() {
+    $('#addEmpFN').val('');
+    $('#addEmpLN').val('');
+    $('#addEmpID').val('');
+    $('#addEmpTitle').val('');
+    $('#addEmpAnnSal').val('');
 }
 
 // appending
@@ -43,7 +56,7 @@ function appendEmployeeList() {
     element.empty();
     for( let employee of empArr ) {
         console.log(employee);
-        element.append(`<tr><td>${employee.fName}</td><td>${employee.lName}</td><td>${employee.id}</td><td>${employee.title}</td><td>${employee.annual}</td></tr>`);
+        element.append(`<tr><td>${employee.fName}</td><td>${employee.lName}</td><td id="empIDTable">${employee.id}</td><td>${employee.title}</td><td>${employee.annual}<button class="deleteEmp">Delete</button></td></tr>`);
     }
 }
 
@@ -53,10 +66,33 @@ function sumSal() {
     console.log('adding all of the salaries');
     let empSal = $('#totalMonth');
     let allEmpSalInt = 0;
-    for( let employee of empArr ) {
-        allEmpSalInt += parseInt(`${employee.annual}`);
         empSal.empty();
-        empSal.append(allEmpSalInt/12);
+        empSal.append('Total Monthly: $', allEmpSalInt.toFixed(2));
+    for( let employee of empArr ) {
+        allEmpSalInt += parseFloat(employee.annual/12);
+        empSal.empty();
+        empSal.append('Total Monthly: $', allEmpSalInt.toFixed(2));
+        if (allEmpSalInt > 20000 ) {
+            $('#totalMonth').toggleClass('over20k');        
+        }
     }
 }
 
+
+// deleting employee
+
+function deleteEmp() {
+    console.log('deleting the employee!');
+    for( let emp in empArr) {
+        console.log(empArr[emp].id);
+        console.log($(this).closest('tr').find('#empIDTable').text());
+        if(empArr[emp].id == $(this).closest('tr').find('#empIDTable').text()){
+            console.log('bye now');
+            empArr.splice(emp, 1);
+        } else {
+            console.log('false');
+        }
+    }
+    appendEmployeeList();
+    sumSal();
+}
